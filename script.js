668c117a27607ab273a783225ceb2a19c16a678a
Kateryna0712 <katerina.shkvaruk@gmail.com>
2022-01-21 17:44:23 +0100

    document write

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let x = 5;
-alert(typeof x);
+// let x = 5;
+// alert(typeof x);
 let cat = {
     name :'Bars',
     age : 2
 };
-alert(typeof cat);
-alert(typeof(cat));
+let userName = "Katya";
+// alert(typeof cat);
+// alert(typeof(cat));
+console.log(cat);
+document.write(userName);
+document.write('<h2>' + userName + '</h2>');
+document.write(`<h1>${userName}</h1>`);
